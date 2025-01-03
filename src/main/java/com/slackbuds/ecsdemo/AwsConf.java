ec06c286b59bbb00f017f6e8849e1afe572fd423
Bruce Downs <bruceadowns@gmail.com>
2025-01-03 13:42:36 -0700

    default region to uw2

--- src/main/java/com/slackbuds/ecsdemo/AwsConf.java
+++ src/main/java/com/slackbuds/ecsdemo/AwsConf.java
@@ -1,9 +1,11 @@
 package com.slackbuds.ecsdemo;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
@@ -16,7 +18,9 @@ public class AwsConf {
 
   @Bean
   public DynamoDbClient dynamoDbClient(AwsCredentialsProvider awsCredentialsProvider) {
+    var region = System.getenv("AWS_REGION");
     return DynamoDbClient.builder()
+        .region(Strings.isBlank(region) ? Region.US_WEST_2 : Region.of(region))
         .credentialsProvider(awsCredentialsProvider)
         .build();
   }
