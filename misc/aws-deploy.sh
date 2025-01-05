e76f6b9a2c20445a4c0f7bf7b9f3bc5dd45a187b
Bruce Downs <bruceadowns@gmail.com>
2025-01-05 09:50:33 -0700

    acm list

--- misc/aws-deploy.sh
+++ misc/aws-deploy.sh
@@ -19,7 +19,10 @@ aws elbv2 describe-load-balancers
 aws elbv2 describe-target-groups
 aws elbv2 describe-listeners --load-balancer-arn arn:aws:elasticloadbalancing:us-west-2:714681258665:loadbalancer/app/ecsdemo-dev/7bc8d4e1a1dbab36
 
-# blog
+# acm
+aws acm list-certificates
+
+# https://medium.com/boltops/gentle-introduction-to-how-aws-ecs-works-with-example-tutorial-cea3d27ce63d
 aws ec2 create-security-group --group-name my-ecs-sg --description my-ecs-sg
 aws ecs register-task-definition --cli-input-json file://task-definition.json
 aws ec2 authorize-security-group-ingress --group-name my-ecs-sg --protocol tcp --port 1-65535 --source-group my-elb-sg
